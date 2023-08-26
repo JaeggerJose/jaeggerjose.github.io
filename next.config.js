0f16c7678b3ec332b888c7e9ad364e16d8f2ee78
Liao Ming-Hsuan <minghsuan@El-relampago.local>
2023-08-26 22:30:20 +0800

    :tada: init a new project of my personal website

---
+++ next.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    reactStrictMode: true,
+}   // This is the default config, so it's not necessary to include it here.
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const options = {
+    arrowParens: 'always',
+    singleQuote: true,
+    backetSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: 4,
+    trailingComma: 'none',
+}
