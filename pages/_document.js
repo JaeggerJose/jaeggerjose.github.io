217153a351bcf7286e66c5db0382402e97cacbe6
JaeggerJose <90586932+JaeggerJose@users.noreply.github.com>
2024-02-10 21:09:30 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,26 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript} from 'next/document';
 import theme from "../libs/theme";
+import { Script } from 'next/script';
 
 export default class Document extends NextDocument {
     render () {
         return (
             <Html lang="en">
                 <Head>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N0YQWYT632"></script>
-                    <script>
+                  <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-N0YQWYT632"
+                    strategy="afterInteractive"
+                  />
+                  <Script id="google-analytics" strategy="afterInteractive">
+                    {`
                       window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
+                      function gtag(){window.dataLayer.push(arguments);}
                       gtag('js', new Date());
-                    
+            
                       gtag('config', 'G-N0YQWYT632');
-                    </script>
+                    `}
+                  </Script>
                 </Head>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
