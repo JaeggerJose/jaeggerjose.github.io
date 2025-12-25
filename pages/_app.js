16eb190af83615852023257c23bbc47cacf7f065
JaeggerJose <lms025187@gmail.com>
2025-12-25 21:52:34 +0800

    update GA config

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layouts/main";
 import theme from "../libs/theme";
 import Fonts from "../components/layouts/font";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
@@ -9,6 +11,7 @@ const Website = ({ Component, pageProps, router }) => {
             <Layout router={router}>
                 <Component {...pageProps} key={router.router} />
             </Layout>
+            <GoogleAnalytics gaId="G-N0YQWYT632" />
         </ChakraProvider>
     )
 }
